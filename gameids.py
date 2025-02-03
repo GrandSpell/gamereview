83fdae25ab8a8e808cf556273f2dd670e0065022
GrandSpell <hirokazu.miyao.dc@gmail.com>
2025-02-03 21:38:10 +0900

    first commit

---
+++ gameids.py
@@ -0,0 +1,62 @@
+def get_gameid():
+
+    gameids={
+"620":"Portal 2",
+"105600":"Terraria",
+"292030":"The Witcher 3: Wild Hunt",
+"294100":"RimWorld",
+"367520":"Hollow Knight",
+"381210":"Dead by Daylight",
+"489830":"The Elder Scrolls V: Skyrim Special Edition",
+"524220":"NieR:Automata",
+"578080":"PUBG: BATTLEGROUNDS",
+"620980":"Beat Saber",
+"740130":"Tales of Arise",
+"945360":"Among Us",
+"1091500":"Cyberpunk 2077",
+"1097150":"Fall Guys",
+"1237970":"Titanfall® 2",
+}
+
+    titledix={}
+    titles=[]
+    for i in gameids:
+        titledix[gameids[i]]=i
+        titles.append(gameids[i])
+    return titledix,titles
+
+
+
+
+"""
+    gameids={
+"740130":"Tales of Arise",
+"1091500":"Cyberpunk 2077",
+"524220":"NieR:Automata",
+"620980":"Beat Saber",
+"367520":"Hollow Knight",
+"2701660":"ドラゴンクエストIII　そして伝説へ…",
+"1295510":"ドラゴンクエストXI　過ぎ去りし時を求めて S",
+"1794680":"Vampire Survivors",
+"582010":"Monster Hunter: World",
+"1446780":"MONSTER HUNTER RISE",
+"1245620":"ELDEN RING"
+}
+"""
+
+
+
+"""
+    gameids={
+"262060":"Darkest Dungeon®",
+"435150":"Divinity: Original Sin 2 - Definitive Edition",
+"489830":"The Elder Scrolls V: Skyrim Special Edition",
+"524220":"NieR:Automata",
+"740130":"Tales of Arise",
+"1091500":"Cyberpunk 2077",
+"1113560":"NieR Replicant",
+"1145360":"Hades",
+"1151640":"Horizon Zero Dawn™ Complete Edition",
+"1325200":"Nioh 2 – The Complete Edition",
+}
+"""
