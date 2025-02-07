b107b21c8db5bd480641f0303ff008709a6ceef2
GrandSpell <hirokazu.miyao.dc@gmail.com>
2025-02-08 00:20:54 +0900

    修正

--- main.py
+++ main.py
@@ -7,6 +7,56 @@ st.set_page_config(
     initial_sidebar_state="expanded",
 )
 
+HIDE_ST_STYLE = """
+                <style>
+                div[data-testid="stToolbar"] {
+                visibility: hidden;
+                height: 0%;
+                position: fixed;
+                }
+                div[data-testid="stDecoration"] {
+                visibility: hidden;
+                height: 0%;
+                position: fixed;
+                }
+                #MainMenu {
+                visibility: hidden;
+                height: 0%;
+                }
+                header {
+                visibility: hidden;
+                height: 0%;
+                }
+                footer {
+                visibility: hidden;
+                height: 0%;
+                }
+				        .appview-container .main .block-container{
+                            padding-top: 1rem;
+                            padding-right: 3rem;
+                            padding-left: 3rem;
+                            padding-bottom: 1rem;
+                        }  
+                        .reportview-container {
+                            padding-top: 0rem;
+                            padding-right: 3rem;
+                            padding-left: 3rem;
+                            padding-bottom: 0rem;
+                        }
+                        header[data-testid="stHeader"] {
+                            z-index: -1;
+                        }
+                        div[data-testid="stToolbar"] {
+                        z-index: 100;
+                        }
+                        div[data-testid="stDecoration"] {
+                        z-index: 100;
+                        }
+                </style>
+"""
+
+st.markdown(const.HIDE_ST_STYLE, unsafe_allow_html=True)
+
 section = st.sidebar.radio(
     "モードを選択:",
     ("基本モード", "比較分析モード")
