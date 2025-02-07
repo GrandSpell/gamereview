c49a312dba11f6681e0993f839b7795d006831b3
GrandSpell <hirokazu.miyao.dc@gmail.com>
2025-02-07 23:55:19 +0900

    修正

--- set_stream2.py
+++ set_stream2.py
@@ -2,7 +2,7 @@ def setstream2(gameid,gamename):
     import streamlit as st
     import numpy as np
     import matplotlib.pyplot as plt
-    import japanize_matplotlib
+    from japanize-matplotlib import japanize_matplotlib
     import streamlit as st
     from PIL import Image
     from io import BytesIO
